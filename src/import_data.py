43597f2453bdb9b5eb80581b2ae6f7fcdbb69739
Jacob A <jacob.ard@snhu.edu>
2026-08-01 17:33:20 -0400

    add todo comment

--- src/import_data.py
+++ src/import_data.py
@@ -14,5 +14,6 @@ from sqlite_data_import import AacDataImporter
 # drop aac database tables if exists,
 # create aac database,
 # import aac dataset and create relational subtables
+# TODO: change test.db to aac.db
 importer = AacDataImporter("test.db", "aac_shelter_outcomes.csv")
 importer.import_data()
